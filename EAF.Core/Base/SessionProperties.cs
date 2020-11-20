97bb5095e5f6ce3799fe74a08035753844aea04f
Bernard Visagie <bernardv@live.co.za>
2020-11-20 11:31:54 +0200

    WIP

---
+++ EAF.Core/Base/SessionProperties.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using Serilog.Core;
+
+namespace EAF.Core.Base
+{
+    internal class SessionProperties
+    {
+        public IWebDriver Driver { get; set; }
+        public Logger Logger { get; set; }
+    }
+}
