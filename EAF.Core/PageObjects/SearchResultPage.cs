4cc8b8f25b1b171d189b5657f357e5aabbcb039e
Bernard <bernard.visagie@kingprice.co.za>
2020-11-23 11:29:26 +0200

    WIP

---
+++ EAF.Core/PageObjects/SearchResultPage.cs
@@ -0,0 +1,14 @@
+﻿using EAF.Core.Base;
+
+namespace EAF.Core.PageObjects
+{
+    public class SearchResultPage
+    {
+        private readonly Session sessionVariables;
+
+        public SearchResultPage(Session sessionVariables)
+        {
+            this.sessionVariables = sessionVariables;
+        }
+    }
+}
